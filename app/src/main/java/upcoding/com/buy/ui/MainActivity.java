ec66524d87ea8dfd21c56f46a11620ce88ed71e7
Heboot <heboot@126.com>
2017-01-12 22:44:56 +0800

    project init
    project init

---
+++ app/src/main/java/upcoding/com/buy/ui/MainActivity.java
@@ -0,0 +1,15 @@
+package upcoding.com.buy.ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import upcoding.com.buy.R;
+
+public class MainActivity extends ToolbarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/upcoding/com/buy/ui/ToolbarActivity.java
@@ -0,0 +1,37 @@
+package upcoding.com.buy.ui;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
+
+import upcoding.com.buy.R;
+
+/**
+ * Created by Heboot on 2017/1/11.
+ */
+
+public class ToolbarActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // 这句很关键，注意是调用父类的方法
+        super.setContentView(R.layout.activity_base_toolbar);
+//        // 经测试在代码里直接声明透明状态栏更有效
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+//            localLayoutParams.flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        }
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (toolbar != null) {
+            setSupportActionBar(toolbar);
+        }
+
+//        initToolbar();
+    }
+
+}
