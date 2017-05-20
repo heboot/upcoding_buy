a059c751a89d02db56df69c775a690f1a3d15178
heboot <heboot@126.com>
2017-05-20 23:42:27 +0800

    databinding test
    databinding test

--- app/src/main/java/com/upcoding/buy/ui/post/PostDetailActivity.java
+++ app/src/main/java/com/upcoding/buy/ui/post/PostDetailActivity.java
@@ -1,4 +1,4 @@
-package com.upcoding.buy.ui.info;
+package com.upcoding.buy.ui.post;
 
 import android.databinding.DataBindingUtil;
 import android.os.Bundle;
@@ -8,7 +8,6 @@ import android.widget.TextView;
 
 import com.upcoding.buy.R;
 import com.upcoding.buy.common.ContentKey;
-import com.upcoding.buy.databinding.ContentDetailBind;
 import com.upcoding.buy.databinding.InfoDetailBind;
 import com.upcoding.buy.model.InfoModel;
 import com.upcoding.buy.ui.ToolbarActivity;
@@ -17,7 +16,7 @@ import com.upcoding.buy.ui.ToolbarActivity;
  * Created by Heboot on 2017/5/3.
  */
 
-public class InfoDetailActivity extends ToolbarActivity {
+public class PostDetailActivity extends ToolbarActivity {
 
     private InfoModel infoModel;
 
@@ -30,18 +29,13 @@ public class InfoDetailActivity extends ToolbarActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-        ContentDetailBind contentDetailBind = DataBindingUtil.setContentView(this, R.layout.layout_content_detail);
+        InfoDetailBind infoDetailBind = DataBindingUtil.setContentView(this, R.layout.activity_info_detail);
         infoModel = (InfoModel) getIntent().getExtras().get(ContentKey.INFO_MODEL);
         if (infoModel != null) {
             initIncludeToolBar(infoModel.getTitle());
-            contentDetailBind.setContentType("info");
-            contentDetailBind.setInfoModel(infoModel);
+            infoDetailBind.setInfoModel(infoModel);
         } else {
         }
-
-        String aa = "a";
-        aa.equals("2");
 
     }
 
