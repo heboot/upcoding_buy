6b553e4b5e506450a7e6877879151d967030b640
heboot <heboot@126.com>
2017-05-29 23:10:38 +0800

    test
    test

--- app/src/main/java/com/upcoding/buy/ui/ToolbarActivity.java
+++ app/src/main/java/com/upcoding/buy/ui/ToolbarActivity.java
@@ -1,5 +1,6 @@
 package com.upcoding.buy.ui;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
@@ -32,12 +33,12 @@ public abstract class ToolbarActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 //        // 这句很关键，注意是调用父类的方法
         super.setContentView(R.layout.activity_base_toolbar);
-////        // 经测试在代码里直接声明透明状态栏更有效
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
-//            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
-//            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
-////            localLayoutParams.flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
-//        }
+//        // 经测试在代码里直接声明透明状态栏更有效
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+//            localLayoutParams.flags = WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS;
+        }
 
         initToolbar();
 
